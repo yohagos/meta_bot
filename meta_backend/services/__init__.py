ff9304f98c7ccd69e63195f7f98c441a646e29e0
yohagos <yosef.hagos@googlemail.com>
2025-03-27 14:12:16 +0100

    update: services and adding websocket manager

--- meta_backend/services/__init__.py
+++ meta_backend/services/__init__.py
@@ -1,1 +1,2 @@
 from .coin_api import fetch_coin_data, coin_api_polling_task
+from .websocket_manager import WebSocketManager
--- meta_backend/services/coin_api.py
+++ meta_backend/services/coin_api.py
@@ -1,14 +1,18 @@
-import asyncio, aiohttp
+import asyncio, aiohttp, json, aio_pika
 from typing import Dict, Any, List
 from sqlmodel import delete, select
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 from uuid import uuid4
 from datetime import datetime
+from fastapi import FastAPI
 
 from database import AsyncSessionLocal
-from models import Stats
-from rabbitmq import publish_coin_data, rabbitmq_channel_pool
+from models.coin_stats import Stats
+from models.rabbitmq_config import RabbitMQConfig
+from rabbitmq import publish_message, RabbitMQConnectionManager
+
+
 from configs import load_settings
 from utils import logger
 
@@ -27,12 +31,16 @@ async def fetch_coin_data() -> Dict[str, Any]:
                     "data": data['data'],
                     "timestamp": data['timestamp']
                 }
-    except aiohttp.ClientError() as e:
+    except aiohttp.ClientError as e:
         logger.error(f"Coin API request failed")
     except KeyError as e:
         logger.error(f"Invalid Coin API response")
     
     return None
+
+async def load_exchange_info(session: AsyncSession) -> RabbitMQConfig:
+    coin_config = await session.get(RabbitMQConfig, "coins")
+    return coin_config
 
 
 async def clear_stats_data(session: AsyncSession):
@@ -41,24 +49,26 @@ async def clear_stats_data(session: AsyncSession):
         await session.commit()
 
 
-async def coin_api_polling_task(session: AsyncSession):
-    while True:
-        start_time = asyncio.get_event_loop().time()
+async def coin_api_polling_task(app: FastAPI, stop_event: asyncio.Event):
+    async with AsyncSessionLocal() as session:
+        config = await load_exchange_info(session)
+    mq_manager = app.state.mq_manager
+    while not stop_event.is_set():
         try: 
-            async with AsyncSessionLocal() as session, rabbitmq_channel_pool() as (connection, channel):
+            start_time = asyncio.get_event_loop().time()
+            await mq_manager.get_connection()
+            
+            async with AsyncSessionLocal() as session:
                 data = await fetch_coin_data()
                 if not data or "data" not in data:
-                    logger.warning("No data or invalid structure received.")
                     await asyncio.sleep(20)
                     continue
 
                 coin_data: List[Any] = data['data']
-                
                 await clear_stats_data(session)
                 timestamp = datetime.fromtimestamp(int(data['timestamp']) / 1e3)
 
                 publishing_data: List[Stats] = []
-                
                 for a in coin_data:
                     try:
                         coin = Stats(
@@ -83,18 +93,37 @@ async def coin_api_polling_task(session: AsyncSession):
 
                     except (KeyError, ValueError, TypeError) as e:
                         logger.info(f"error for coin {a.get('id', 'unknown')}: {str(e)}")
+                    await asyncio.sleep(0)
                 
-                try:
-                    await session.commit()
-                    await publish_coin_data(channel, publishing_data)
-                except SQLAlchemyError as se:
-                    await session.rollback()
-                    logger.error(f"Commit failed: {str(se)}")
-        
-        except Exception as e:
-            logger.error(f"Critical error : {str(e)}", exc_info=True)
+                await session.commit()
+                
+                payload = json.dumps(
+                    [item.model_dump() for item in publishing_data], 
+                    default=str
+                ).encode()
+                
+                await publish_message(
+                    manager=mq_manager,
+                    config=config,
+                    message_body=payload
+                )
 
-        elapsed = asyncio.get_event_loop().time() - start_time
-        sleep_time = max(0, 20 - elapsed)
-        await asyncio.sleep(sleep_time)
+            elapsed = asyncio.get_event_loop().time() - start_time
+            sleep_time = max(0, 20 - elapsed)
+            while sleep_time > 0 and not stop_event.is_set():
+                await asyncio.sleep(min(1, sleep_time))
+                sleep_time -= 1
+                
+        except (asyncio.CancelledError, KeyboardInterrupt) as e:
+            stop_event.set()
+            break
+        except (aio_pika.exceptions.AMQPConnectionError, aio_pika.exceptions.AMQPError) as e:
+            logger.warning(f"RabbitMQ connection error: {str(e)}. Retrying in 2 seconds.")
+            await asyncio.sleep(1)
+        except SQLAlchemyError as se:
+            logger.error(f"Database error: {str(se)}")
+            await asyncio.sleep(1)
+        except Exception as e:
+            logger.error(f"Polling error: {str(e)}", exc_info=True)
+            await asyncio.sleep(1)
         
