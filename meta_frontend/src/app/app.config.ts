736302bc43332a4920ae9b5d3bc8dd8ddb816484
yohagos <yosef.hagos@googlemail.com>
2025-04-02 15:30:25 +0200

    update: gitignore and added dependency for visualizing charts

--- meta_frontend/src/app/app.config.ts
+++ meta_frontend/src/app/app.config.ts
@@ -4,12 +4,35 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DATE_PIPE_DEFAULT_OPTIONS, DatePipe } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { includeBearerTokenInterceptor } from "keycloak-angular";
 import { provideKeycloakAngular } from './keycloak.config';
 import { tokenInterceptor } from './core/interceptor/token.interceptor';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+import * as echarts from 'echarts';
+import {
+  LineChart,
+  LineSeriesOption,
+  BarChart,
+  BarSeriesOption,
+  LinesChart,
+  LinesSeriesOption,
+} from 'echarts/charts';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  LegendComponent,
+} from "echarts/components";
+import {
+  CanvasRenderer
+} from "echarts/renderers";
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+
 
 
 
@@ -19,6 +42,9 @@ const providers = [
   CommonModule,
   BrowserModule,
   BrowserAnimationsModule,
+  NgxEchartsModule.forRoot({
+    echarts: () => Promise.resolve(echarts)
+  }),
 ]
 
 export const appConfig: ApplicationConfig = {
@@ -27,6 +53,10 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideKeycloakAngular(),
     provideHttpClient(withInterceptors([includeBearerTokenInterceptor, tokenInterceptor])),
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: { timezone: 'UTC' }
+    },
     importProvidersFrom(providers),
   ]
 };
